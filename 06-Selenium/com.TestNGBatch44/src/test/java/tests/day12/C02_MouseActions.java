6aed55f679273e39d1fd2794e5de041960012347
bulutluoz <abbulutluoz@gmail.com>
2022-01-28 20:14:41 +0100

    11.gun

---
+++ 06-Selenium/com.TestNGBatch44/src/test/java/tests/day12/C02_MouseActions.java
@@ -0,0 +1,10 @@
+package tests.day12;
+
+public class C02_MouseActions {
+    // amazon anasayfaya gidin
+    // sag ustte hello,signIn elementinin uzerinde mouse'u bekletin
+    // acilan menude new list linkine tiklayin
+    // ve new list sayfasinin acildigini test edin
+
+
+}
