83bbad930f85303c461fdd5214b1e80d50154515
bulutluoz <abbulutluoz@gmail.com>
2022-02-12 20:17:50 +0100

    herokuapp

--- 06-Selenium/com.CucumberBatch44/src/test/java/runners/Runner.java
+++ 06-Selenium/com.CucumberBatch44/src/test/java/runners/Runner.java
@@ -7,10 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
         features="src/test/resources/features",
         glue = "stepdefinitions",
-        tags="@Editor",
-        dryRun = true
+        tags="@Hero",
+        dryRun = false
 )
 
 public class Runner {
